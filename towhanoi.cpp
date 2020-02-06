4e4e412578d41760dfc80638282790c0b57b5a8f
bhushithgagan <42802614+bhushithgagan@users.noreply.github.com>
2020-02-06 11:51:24 +0530

    Add files via upload

---
+++ towhanoi.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void towhan(int n, char src, char temp, char dest) {
+	if (n==1) {
+	cout<<"Disc"<<" "<<n<<" "<<"moved from"<<" "<<src<<" "<<"to"<<" "<<dest<<endl;
+		return;
+	}
+	towhan(n-1,src,dest,temp);
+	cout<<"Disc"<<" "<<n<<" "<<"moved from"<<" "<<src<<" "<<"to"<<" "<<dest<<endl;
+	towhan(n-1,temp,src,dest);
+}
+
+int main() {
+	int n;
+	char src = 'a', dest = 'b', temp = 'c';
+	cout<<"Enter the number of discs:"<<endl;
+	cin>>n;
+	towhan(n,src,temp,dest);
+	cout<<"Discs moved from source to destination!"<<endl;
+	return 0;
+}
+
+
+
